5e9815e47a3331fff25420dc05079d4c4d63edd0
www.axisnepal.com <103080112+duldines@users.noreply.github.com>
2024-12-27 23:40:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// How to acceptuser input?
+// 1 Esasy way = window prompt
+// 2 professional way = HTML TEXTBOX//
+
+
+// //Esay way through window.promt
+// let userName;
+// userName = window.prompt("what is your name?");
+// console.log(userName);
+
+constweatherForm = document.querySelector('.weather');
+const cityInput = document.querySelecto('cityInput');
